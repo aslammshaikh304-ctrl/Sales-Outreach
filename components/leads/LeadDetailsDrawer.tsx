9f7c9180585186fe3db58df85e0a584e7e3e3333
Aslam Shaikh <aslammshaikh304@gmail.com>
2026-07-15 11:52:41 +0530

    Delete button

--- components/leads/LeadDetailsDrawer.tsx
+++ components/leads/LeadDetailsDrawer.tsx
@@ -315,14 +315,7 @@ export default function LeadDetailsDrawer({
                   >
                     Edit
                   </button>
-
-                  <button
-                    onClick={handleDelete}
-                    disabled={deleting}
-                    className="rounded-lg bg-red-600 px-4 py-2 text-sm font-medium text-white hover:bg-red-500 disabled:opacity-50"
-                  >
-                    {deleting ? "Deleting..." : "Delete"}
-                  </button>
+              
                 </>
               )}
 
